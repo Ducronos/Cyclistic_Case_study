7605f0b72ff0270bdb4779e82d7af443810edbbe
Ducronos <ducnguyenminh2902@gmail.com>
2026-01-31 15:49:50 +0700

    Add gitignore and update analysis report

--- process_code.R
+++ process_code.R
@@ -6,6 +6,8 @@ library(skimr)
 library(lubridate)
 library(patchwork)
 library(purrr)
+library(arrow)
+library(writexl)
 
 #rename the data columns for more convinient analysis
 trip_2019 <- read_csv("D:/Learning/data_analyst/Google_data_analytics/Case_study_Cyclistic/Datasets/trip_info_2019_duration_fix.csv")
@@ -405,38 +407,50 @@ trip_2020 %>%
   )
 
 #Check for distribution of user_type per route
-member_route <- trip_2019 %>% 
+member_route_2019 <- trip_2019 %>% 
   filter(user_type == "member") %>% 
   count(route_id) %>% 
   mutate(
     pct = n/sum(n)*100
   )
 
-casual_route <- trip_2019 %>% 
+casual_route_2019 <- trip_2019 %>% 
   filter(user_type == "casual") %>% 
   count(route_id) %>% 
   mutate(
     pct = n/sum(n)*100
   )
 
-ggplot() +
+p1 <- ggplot() +
   geom_histogram(
-    data = member_route,
+    data = member_route_2019,
     aes(x = pct),
     alpha = 0.6,
     bins = 50,
+  ) + 
+  labs(
+    title = "Distribution of member route portion",
+    x = "Percentage of trips per route (%)",
+    y = "Number of routes"
   )
 
-ggplot() +
+p2 <- ggplot() +
   geom_histogram(
-    data = casual_route,
+    data = casual_route_2019,
     aes(x = pct),
     alpha = 0.6,
     bins = 50,
+  ) +
+  labs(
+    title = "Distribution of casual route portion",
+    x = "Percentage of trips per route (%)",
+    y = "Number of routes"
   )
+
+p1/p2
 
 #Validate IQR index
-iqr_route_casual <- casual_route %>% 
+iqr_route_casual_2019 <- casual_route_2019 %>% 
   summarize(
     user_type = "casual",
     med = median(pct),
@@ -446,7 +460,7 @@ iqr_route_casual <- casual_route %>%
     max_route = max(pct)
   )
 
-iqr_route_member <- member_route %>% 
+iqr_route_member_2019 <- member_route_2019 %>% 
   summarize(
     user_type = "member",
     med = median(pct),
@@ -457,7 +471,261 @@ iqr_route_member <- member_route %>%
   )
 
 iqr_route_2019 <- bind_rows(
-  iqr_route_member,
-  iqr_route_casual
+  iqr_route_member_2019,
+  iqr_route_casual_2019
 )
+
 iqr_route_2019
+
+#Check for distribution of user_type per route
+member_route_2020 <- trip_2020 %>% 
+  filter(user_type == "member") %>% 
+  count(route_id) %>% 
+  mutate(
+    pct = n/sum(n)*100
+  )
+
+casual_route_2020 <- trip_2020 %>% 
+  filter(user_type == "casual") %>% 
+  count(route_id) %>% 
+  mutate(
+    pct = n/sum(n)*100
+  )
+
+p1 <- ggplot() +
+  geom_histogram(
+    data = member_route_2020,
+    aes(x = pct),
+    alpha = 0.6,
+    bins = 50,
+  ) + 
+  labs(
+    title = "Distribution of member route portion",
+    x = "Percentage of trips per route (%)",
+    y = "Number of routes"
+  )
+
+p2 <- ggplot() +
+  geom_histogram(
+    data = casual_route_2020,
+    aes(x = pct),
+    alpha = 0.6,
+    bins = 50,
+  ) +
+  labs(
+    title = "Distribution of casual route portion",
+    x = "Percentage of trips per route (%)",
+    y = "Number of routes"
+  )
+
+p1/p2
+
+#Validate IQR index
+iqr_route_casual_2020 <- casual_route_2020 %>% 
+  summarize(
+    user_type = "casual",
+    med = median(pct),
+    q1 = quantile(pct, 0.25, na.rm = 0),
+    q3 = quantile(pct, 0.75, na.rm = 0),
+    iqr = q3 - q1,
+    max_route = max(pct)
+  )
+
+iqr_route_member_2020 <- member_route_2020 %>% 
+  summarize(
+    user_type = "member",
+    med = median(pct),
+    q1 = quantile(pct, 0.25, na.rm = 0),
+    q3 = quantile(pct, 0.75, na.rm = 0),
+    iqr = q3 - q1,
+    max_route = max(pct)
+  )
+
+iqr_route_2020 <- bind_rows(
+  iqr_route_member_2020,
+  iqr_route_casual_2020
+)
+
+iqr_route_2020
+
+prep_for_powerbi <- function(df) {
+  df %>%
+    mutate(
+      across(where(is.factor), as.character),
+      across(where(is.logical), as.integer),
+      across(where(~ inherits(.x, "Date")), ~ format(.x, "%Y-%m-%d")),
+      across(where(~ inherits(.x, "POSIXct")), ~ format(.x, "%Y-%m-%d %H:%M:%S"))
+    ) %>%
+    janitor::clean_names()
+}
+
+iwalk(
+  dfs,
+  ~ write_csv(
+    prep_for_powerbi(.x),
+    paste0("bi/", .y, ".csv")
+  )
+)
+
+dfs = list(
+  member_route = member_route,
+  casual_route = casual_route,
+  entire_data,
+  casual_route_2019 = casual_route_2019,
+  member_route_2019 = member_route_2019,
+  casual_route_2020 = casual_route_2020,
+  member_route_2020 = member_route_2020,
+  pct_vld_2019 = pct_vld_2019,
+  pct_vld_2020 = pct_vld_2020,
+  trip_2019 = trip_2019,
+  trip_2019_demo = trip_2019_demo,
+  trip_2020 = trip_2020
+)
+
+info_2019 <- trip_2019 %>% mutate(year = 2019, trip_id = as.character(trip_id))
+info_2020 <- trip_2020 %>% mutate(year = 2020, trip_id = as.character(trip_id))
+
+common_data <- intersect(names(info_2019), names(info_2020))
+
+entire_data <- bind_rows(
+  info_2019 %>% select(all_of(common_data)),
+  info_2020 %>% select(all_of(common_data))
+)
+
+member_route <- bind_rows(
+  member_route_2019,
+  member_route_2020
+)
+
+casual_route <- bind_rows(
+  casual_route_2019,
+  casual_route_2020
+)
+
+p1 <- ggplot() +
+  geom_histogram(
+    data = member_route,
+    aes(x = pct),
+    alpha = 0.6,
+    bins = 50,
+  ) + 
+  labs(
+    title = "Distribution of member route portion",
+    x = "Percentage of trips per route (%)",
+    y = "Number of routes"
+  )
+
+p2 <- ggplot() +
+  geom_histogram(
+    data = casual_route,
+    aes(x = pct),
+    alpha = 0.6,
+    bins = 50,
+  ) +
+  labs(
+    title = "Distribution of casual route portion",
+    x = "Percentage of trips per route (%)",
+    y = "Number of routes"
+  )
+
+p1/p2
+
+entire_data <- entire_data %>%
+  mutate(
+    duration_log10    = log10(duration_sec + 1)
+  )
+
+route_summary <- entire_data %>% 
+  group_by(user_type, route_id) %>% 
+  summarise(
+    trip_count = n(),
+    .groups = "drop"
+  )
+
+route_ranked <- route_summary %>% 
+  group_by(user_type) %>% 
+  arrange(desc(trip_count)) %>% 
+  mutate(
+    route_rank = row_number(),
+    trip_pct = trip_count / sum(trip_count),
+    cum_trip_pct = cumsum(trip_pct)
+  ) %>% 
+  ungroup()
+
+route_ranked <- route_ranked %>% 
+  select(user_type, route_rank, cum_trip_pct)
+
+dfs = list(
+  route_ranked = route_ranked
+)
+
+trip_2019_demo <- trip_2019_demo %>% 
+  mutate(
+    age_group = cut(
+      age,
+      breaks = c(0, 18, 25, 35, 45, 55, 65, 120),
+      right = FALSE
+    ),
+    age_rank = case_when(
+      age_group == "[0,18)" ~ 1,
+      age_group == "[18,25)" ~ 2,
+      age_group == "[25,35)" ~ 3,
+      age_group == "[35,45)" ~ 4,
+      age_group == "[45,55)" ~ 5,
+      age_group == "[55,65)" ~ 6,
+      age_group == "[65,120)" ~ 7
+    ),
+    duration_log10 = log10(duration_sec +1)
+  ) %>% 
+  select(trip_id, user_type, age_group, age_rank, duration_sec, duration_log10)
+
+dfs = list(
+  trip_2019_demo = trip_2019_demo
+)
+
+kpi_50pct <- route_ranked %>% 
+  filter(cum_trip_pct >= 0.5) %>% 
+  group_by(user_type) %>% 
+  summarise(
+    routes_for_50pct = min(route_rank),
+    .groups = "drop"
+  )
+
+kpi_top_routes <- route_ranked %>% 
+  filter(route_rank <= 10) %>% 
+  group_by(user_type) %>% 
+  summarise(
+    pct_trip_top10 = sum(trip_pct),
+    .groups = "drop"
+  )
+
+kpi_summary <- left_join(kpi_50pct, kpi_top_routes, by = "user_type")
+
+dfs = list(
+  kpi_summary = kpi_summary
+)
+
+add_iqr <- function(df){
+  df %>% 
+    group_by(user_type) %>%
+    summarise(
+      q1 = quantile(duration_sec, 0.25, na.rm = TRUE),
+      med = median(duration_sec, na.rm = TRUE),
+      q3 = quantile(duration_sec, 0.75, na.rm = TRUE),
+      iqr = q3 - q1,
+      lower = q1 - 1.5 * iqr,
+      upper = q3 + 1.5 * iqr
+    )
+}
+
+df_list <- list(
+  entire_data = entire_data
+)
+
+df_list <- map(df_list, add_iqr)
+
+iqr_entire_data <- df_list$entire_data
+
+dfs = list(
+  iqr_entire_data = iqr_entire_data
+)
